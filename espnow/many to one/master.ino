1279dcc39c960989d2422874492375dc91396896
Pchutikarn <151811538+Pchutikarn@users.noreply.github.com>
2024-02-20 18:55:37 +0700

    Revert "10slave and master send data to google sheet"

--- espnow/many to one/master.ino
+++ espnow/many to one/master.ino
@@ -1,195 +1,72 @@
-// Master (แม่ค่าย , ตัวรับ)
+//Master (แม่ค่าย , ตัวรับ)
 #include <ESP8266WiFi.h>
-#include <WiFiClientSecure.h>
 #include <espnow.h>
 
-const char *ssid = "AMM-Guest";
-const char *password = "FM@AMM2019";
-
-const char *host = "script.google.com";
-const int httpsPort = 443;
-String GAS_ID = "";
-
-WiFiClientSecure client;
-
-unsigned long now = millis();
-unsigned long lastMeasure = 0;
-const unsigned long interval = 60 * 60 * 1000;
-
-typedef struct struct_message
-{
-  int id;
-  float temp;
-  float humi;
-  float soilPer;
+// Structure example to receive data
+// Must match the sender structure
+typedef struct struct_message {
+    int id;
+    float temp;
+    float humi;
+    float soilPer;
 } struct_message;
 
+// Create a struct_message called myData
 struct_message myData;
 
-struct_message boardsStruct[10]; // Array to store data from 10 slaves
+// Create a structure to hold the readings from each board
+struct_message board1;
+struct_message board2;
 
-void OnDataRecv(uint8_t *mac_addr, uint8_t *incomingData, uint8_t len)
-{
+// Create an array with all the structures
+struct_message boardsStruct[2] = {board1, board2};
+
+// Callback function that will be executed when data is received
+void OnDataRecv(uint8_t * mac_addr, uint8_t *incomingData, uint8_t len) {
   char macStr[18];
   Serial.print("Packet received from: ");
   snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
            mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
   Serial.println(macStr);
-
   memcpy(&myData, incomingData, sizeof(myData));
-
-  if (myData.id >= 1 && myData.id <= 10)
-  {
-    boardsStruct[myData.id - 1].temp = myData.temp;
-    boardsStruct[myData.id - 1].humi = myData.humi;
-    boardsStruct[myData.id - 1].soilPer = myData.soilPer;
-
-    Serial.printf("Board ID %u: %u bytes\n", myData.id, len);
-    Serial.printf("temp: %f \n", boardsStruct[myData.id - 1].temp);
-    Serial.printf("humi: %f \n", boardsStruct[myData.id - 1].humi);
-    Serial.printf("soil per: %f \n", boardsStruct[myData.id - 1].soilPer);
-    Serial.println();
-  }
-  else
-  {
-    Serial.println("Invalid Board ID");
-  }
-}
-
-void reConnectToWiFi()
-{
-  WiFi.begin(ssid, password);
-  Serial.println("");
-  Serial.print("Re-Connecting");
-  int attemptCount = 0;
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print(".");
-    delay(250);
-    if (++attemptCount > 30)
-    {
-      Serial.println("WiFi connection failed. Please check credentials or router. (In Re Connect To WiFi)");
-      return;
-    }
-  }
-  Serial.println("");
-  Serial.print("Successfully connected to : ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-  Serial.println();
-  client.setInsecure();
-}
-
-void sendDataToGoogle(struct_message boardsData[])
-{
-  Serial.println("==========");
-  Serial.print("Connecting to ");
-  Serial.println(host);
-
-  if (!client.connect(host, httpsPort))
-  {
-    Serial.println("Connection failed");
-    return;
-  }
-
-  String url = "/macros/s/" + GAS_ID + "/exec?";
-
-  for (int i = 0; i < 10; i++)
-  {
-    url += "temp" + String(i + 1) + "=" + String(boardsData[i].temp) +
-           "&humi" + String(i + 1) + "=" + String(boardsData[i].humi) +
-           "&soilPer" + String(i + 1) + "=" + String(boardsData[i].soilPer);
-
-    if (i < 9)
-    {
-      url += "&";
-    }
-  }
-
-  Serial.print("Requesting URL: ");
-  Serial.println(url);
-
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" +
-               "User-Agent: ESP8266\r\n" +
-               "Connection: close\r\n\r\n");
-
-  Serial.println("Request sent");
-
-  while (client.connected())
-  {
-    String line = client.readStringUntil('\n');
-    if (line == "\r")
-    {
-      Serial.println("Headers received");
-      break;
-    }
-  }
-
-  String line = client.readStringUntil('\n');
-  if (line.startsWith("{\"state\":\"success\""))
-  {
-    Serial.println("Success: Data sent to Google Sheet!");
-  }
-  else
-  {
-    Serial.println("Failed to send data to Google Sheet");
-  }
-
-  Serial.print("Reply was: ");
-  Serial.println(line);
-  Serial.println("Closing connection");
-  Serial.println("==========");
+  Serial.printf("Board ID %u: %u bytes\n", myData.id, len);
+  // Update the structures with the new incoming data
+  boardsStruct[myData.id-1].temp = myData.temp;
+  boardsStruct[myData.id-1].humi = myData.humi;
+  boardsStruct[myData.id-1].soilPer = myData.soilPer;
+   Serial.printf("temp: %f \n", boardsStruct[myData.id-1].temp);
+  Serial.printf("humi: %f \n", boardsStruct[myData.id-1].humi);
+  Serial.printf("soil per: %f \n", boardsStruct[myData.id-1].soilPer);
   Serial.println();
 }
 
-void setup()
-{
+
+ 
+void setup() {
+  // Initialize Serial Monitor
   Serial.begin(115200);
+  
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
 
-  WiFi.begin(ssid, password);
-  Serial.println("");
-
-  Serial.print("Connecting");
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print(".");
-    delay(250);
-  }
-
-  Serial.println("");
-  Serial.print("Successfully connected to : ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-  Serial.println();
-
-  client.setInsecure();
-
-  if (esp_now_init() != 0)
-  {
+  // Init ESP-NOW
+  if (esp_now_init() != 0) {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
+  
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info
   esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
   esp_now_register_recv_cb(OnDataRecv);
 }
 
-void loop()
-{
-  now = millis();
-
-  if (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.println("WiFi connection lost. Reconnecting...");
-    reConnectToWiFi();
-  }
-
-  if (now - lastMeasure > interval)
-  {
-    lastMeasure = now;
-
-    sendDataToGoogle(boardsStruct);
-  }
+void loop(){
+  // Access the variables for each board
+  /*int board1X = boardsStruct[0].x;
+  int board1Y = boardsStruct[0].y;
+  int board2X = boardsStruct[1].x;
+  int board2Y = boardsStruct[1].y;
+  */
 }
--- espnow/many to one/slave.ino
+++ espnow/many to one/slave.ino
@@ -1,75 +1,100 @@
-// Slave (ตัวส่ง , ทาส)
+//Slave (ตัวส่ง , ทาส)
 
+#include <ESP8266WiFi.h>
 #include <espnow.h>
+
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 
-const int BOARD_ID = 10;
+// REPLACE WITH RECEIVER MAC Address
 uint8_t broadcastAddress[] = {0x80, 0x7D, 0x3A, 0x37, 0x72, 0x2B};
-typedef struct struct_message
-{
+
+// Set your Board ID (ESP32 Sender #1 = BOARD_ID 1, ESP32 Sender #2 = BOARD_ID 2, etc)
+#define BOARD_ID 1
+
+// Structure example to send data
+// Must match the receiver structure
+typedef struct struct_message {
   int id;
   float temp;
   float humi;
   float soilPer;
 } struct_message;
+
+// Create a struct_message called test to store variables to be sent
 struct_message myData;
+
+unsigned long lastTime = 0;
+unsigned long timerDelay = 10000;
+
+
+
+
 
 #define BME_SCK 13
 #define BME_MISO 12
 #define BME_MOSI 11
 #define BME_CS 10
+
 #define SEALEVELPRESSURE_HPA (1013.25)
-Adafruit_BME280 bme;
+
+Adafruit_BME280 bme; // I2C
+// Adafruit_BME280 bme(BME_CS); // hardware SPI
+// Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK); // software SPI
 
 #define ON_Board_LED 2
 
+// float temp;
+// float humi;
+// int soilPer;
 int valSoil;
 unsigned status;
-long now = millis();
-long lastMeasure = 0;
-// #define DEEP_SLEEP_DURATION_MS (1e6 * 60 * 60)
-#define DEEP_SLEEP_DURATION_MS (1e6 * 59 * 60 + 57 * 1e3)
 
 
-void DeepSleep()
-{
-  Serial.println(F("I'm awake, but I'm going into deep sleep mode for a hour"));
-  Serial.flush();
-  ESP.deepSleep(DEEP_SLEEP_DURATION_MS);
-}
 
-void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus)
-{
+// Callback when data is sent
+void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
   Serial.print("\r\nLast Packet Send Status: ");
-  if (sendStatus == 0)
-  {
+  if (sendStatus == 0) {
     Serial.println("Delivery success");
-    DeepSleep();
+     Serial.println("I'm awake, but I'm going into deep sleep mode for 15 seconds");
+    ESP.deepSleep(15e6);
   }
-  else
-  {
+  else {
     Serial.println("Delivery fail");
   }
 }
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
 
-  if (esp_now_init() != 0)
-  {
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+
+  // Init ESP-NOW
+  if (esp_now_init() != 0) {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
-
+  // Set ESP-NOW role
   esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+
+  // Once ESPNow is successfully init, we will register for Send CB to
+  // get the status of Trasnmitted packet
   esp_now_register_send_cb(OnDataSent);
+
+  // Register peer
   esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
 
+
+
+
+
   status = bme.begin(0x76);
+  // You can also pass in a Wire library object like &Wire2
+  // status = bme.begin(0x76, &Wire2)
   if (!status)
   {
     Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
@@ -82,14 +107,18 @@ void setup()
     while (1)
       delay(10);
   }
+
   Serial.println("-- Default Test --");
 
   pinMode(ON_Board_LED, OUTPUT);
   digitalWrite(ON_Board_LED, HIGH);
+
 }
 
-void loop()
-{
+long now = millis();
+long lastMeasure = 0;
+
+void loop() {
   now = millis();
 
   if (now - lastMeasure > 3000)
@@ -116,6 +145,17 @@ void loop()
     Serial.print(myData.soilPer);
     Serial.println(" %");
 
-    esp_now_send(0, (uint8_t *)&myData, sizeof(myData));
+
+
+
+
+
+    // Send message via ESP-NOW
+    esp_now_send(0, (uint8_t *) &myData, sizeof(myData));
+   
+
   }
+
+
+
 }
